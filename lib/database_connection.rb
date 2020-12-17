2e5073187215fcebf287165c0dac920d95273cfb
AJ <aj.monty@gmail.com>
2020-12-17 17:39:40 +0000

    more DatabaseConnection tests and stuff

--- lib/database_connection.rb
+++ lib/database_connection.rb
@@ -3,11 +3,15 @@ require 'pg'
 class DatabaseConnection
 
     def self.setup(db_name)
-        @connection = PG.connect :dbname => "#{db_name}"
+        @connection = PG.connect(dbname: db_name)
+    end
+
+    def self.connection
+        @connection
     end
 
     def self.query(query_string)
-        result = @connection.exec("#{query_string}")
+        result = @connection.exec(query_string)
     end
 
 end
--- spec/units/database_connection_spec.rb
+++ spec/units/database_connection_spec.rb
@@ -7,8 +7,14 @@ describe DatabaseConnection do
   end
 
   describe '.setup' do
-    it 'has a method called setup that takes 1 argument' do
-      expect(DatabaseConnection).to respond_to(:setup).with(1).argument
+    it 'sets up a connection to a database through PG' do
+      expect(PG).to receive(:connect).with(dbname: 'bookmark_manager_test')
+      DatabaseConnection.setup('bookmark_manager_test')
+    end
+
+    it 'this connection is persistent' do
+      connection = DatabaseConnection.setup('bookmark_manager_test')
+      expect(DatabaseConnection.connection).to eq connection
     end
   end
 
